5c6316d2a1a9bb88aeadc0b688be5ec1f76f0837
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-09-26 12:27:42 -0400

    working in js route

--- Apiroutes.js
+++ Apiroutes.js
@@ -1,9 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const saveData = require("../db/saveData");
 
-const saveData = require('../saveData');
-
-router.get('/notes', function (req, res){
+//get route
+router.get('/notes', (req, res) => {
     saveData
-    .retriveNote()
-    .then(notes => res.json(notes))
+        .getNotes()
+        .then((notes => { 
+            return res.json(notes);
 })
+    .catch((err) => {
+        res.status(500).json(err);
+    })
+    });
+//post routes
+router.post("/notes", (req, res) => {
+    saveData
+    .addNotes(req.body)
+    .then((note) => {
+        res.json(note);
+    })
+    .catch((err)=> {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
