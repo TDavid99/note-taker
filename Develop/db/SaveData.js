52b5ca72c9df4277d9fa2dbf53c406e2e6c7230f
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-09-28 11:34:12 -0400

    read me

--- Develop/db/SaveData.js
+++ Develop/db/SaveData.js
@@ -1,44 +1,44 @@
 const fs = require("fs");
 const util = require("util");
-const uuid = require("../Helper/uuid");
+const uuid = require("../Helper/uuid.js");
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 //read write and get notes and add notes
-const Save {
+class Save {
     read() {
-        return readFile("db/db.json","utf-8");
+        return readFile("db/db.json", "utf-8");
     }
     write(note); {
-       return writeFileAsync("db/db.json", JSON.stringify(note));
-    }
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+}
 getNotes() {
     return this.read().then((notes) => {
         let parsedNotes;
         try {
             parsedNotes = [].concat(JSON.parse(notes));
-         } catch (error) {
-            parsedNotes=[];
-         
-    });
+        } catch (error) {
+            parsedNotes = [];
+
+        });
 }
 
 aadNotes(note) {
-    const {title, text } = note;
-    if(!title || !text) {
+    const { title, text } = note;
+    if (!title || !text) {
         throw new error("please type title ans text");
     }
     const newNote = {
         title,
         text,
-        id:uuid()
+        id: uuid()
 
     };
     return this.getNotes()
-    .then(notes => [...notes, newNote])
-    .then(updatedNotes => this.write(updatedNotes))
-    .then(() => newNote);
+        .then(notes => [...notes, newNote])
+        .then(updatedNotes => this.write(updatedNotes))
+        .then(() => newNote);
 }
 };
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Note Taker Starter Code
+# Note Taker 
+
+This application allows the user to save mulitple notes titles and text. For diffenrent events. currents notes are saved in the left-hand column. The data stays after the page refresh.
+
