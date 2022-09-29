95e6a1a91e1fbe4d00c4e6e80af0af34c241f1ba
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-09-28 23:05:17 -0400

    cleaning up code to see if it links to starter page

--- sever.js
+++ sever.js
@@ -1,7 +1,22 @@
 const express = require('express');
-
-// route files
-const Apiroutes = require("./Apiroutes");
+const path = require("path");
+const fs = require("fs");
+const Apiroutes = require("./Routes/Apiroutes");
 const Routeshtml = require("./Routes/Routeshtml");
 
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/APi", Apiroutes);
+app.use("/", Routeshtml);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () =>
+console.log (`App listening at http://localhost:${PORT}`)
+);
