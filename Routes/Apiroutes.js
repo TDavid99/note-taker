587518e48c33def577e4204c3dd2075ead06522e
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-23 22:02:07 -0500

    working in routes

--- Routes/Apiroutes.js
+++ Routes/Apiroutes.js
@@ -11,9 +11,8 @@ router.get("/notes", (req, res) => {
             console.log (notes)
             return res.json(notes);
 })
-    .catch((err) => {
-        res.status(500).json(err);
-    });
+    .catch((err) => 
+        res.status(500).json(err));
     });
 //post routes
 router.post("/notes", (req, res) => {
@@ -30,22 +29,10 @@ router.post("/notes", (req, res) => {
 
 // bonus Delete button 
 
-function deletesNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-      let notes = notesArray[i];
-  
-      if (notes.id == id) {
-        notesArray.splice(i, 1);
-        fs.writeFileSync(
-          path.join(__dirname, "../db/db.json"),
-          JSON.stringify(notesArray, null, 2)
-        );
-        break;
-      }}}
-      router.delete("/api/notes/:id", (req, res) => {
-        deletesNote(req.params.id, notes);
-        res.json(true);
-      });
+//       router.delete("/api/notes/:id", (req, res) => {
+//         deletesNote(req.params.id, notes);
+//         res.json(true);
+//       });
     
 
 module.exports = router;
